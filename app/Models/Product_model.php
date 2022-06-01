b53b07afcb5da133f86785f9324a35d1aa872482
Tuna Çağlar Gümüş <tunacaglargumus@gmail.com>
2022-06-01 20:55:44 +0300

    Update Product_model.php

--- app/Models/Product_model.php
+++ app/Models/Product_model.php
@@ -1,51 +1,59 @@
 <?php
+
 namespace App\Models;
 
 use CodeIgniter\Model;
 
-class Product_model extends Model {
+class Product_model extends Model
+{
 
-	public function __construct()
+	public function register_product_to_company($product)
 	{
 		$db = db_connect();
+		$builder = $db->table('t_prdct');
+		$builder->insert($product);
 	}
 
-	public function register_product_to_company($product){
-		$db->insert('t_prdct', $product);
-	}
-
-	public function get_product_list($id){
-
+	public function get_product_list($id)
+	{
 		$db = db_connect();
 		$builder = $db->table('t_prdct');
 		$builder->select("*");
-		$builder->where("t_prdct.cmpny_id",$id);
+		$builder->where("t_prdct.cmpny_id", $id);
 		$query = $builder->get();
 		return $query->getResultArray();
-		
 	}
 
-	public function get_product_by_cid_pid($companyID,$product_id){
-		$db->select("*");
-		$db->from("t_prdct");
-		$db->where("t_prdct.cmpny_id",$companyID);
-		$db->where("t_prdct.id",$product_id);
-		$query = $db->get();
-		return $query->row_array();
+	public function get_product_by_cid_pid($companyID, $product_id)
+	{
+		$db = db_connect();
+		$builder = $db->table('t_prdct');
+		$builder->select('*');
+		$builder->where("t_prdct.cmpny_id", $companyID);
+		$builder->where("t_prdct.id", $product_id);
+		$query = $builder->get();
+		return $query->getRowArray();
 	}
 
-	public function set_product($data){
-		$db->insert('t_prdct',$data);
+	public function set_product($data)
+	{
+		$db = db_connect();
+		$builder = $db->table('t_prdct');
+		$builder->insert($data);
 	}
 
-	public function delete_product($id){
-		$db->where('id', $id);
-		$db->delete('t_prdct'); 
+	public function delete_product($id)
+	{
+		$db = db_connect();
+        $builder = $db->table('t_prdct');
+        $builder->delete(['id' => $id]);
 	}
 
-	public function update_product($companyID,$product_id,$productArray){
-    $db->where('t_prdct.cmpny_id',$companyID);   
-    $db->where('t_prdct.id',$product_id);   
-    $db->update('t_prdct',$productArray); 
+	// TODO
+	public function update_product($companyID, $product_id, $productArray)
+	{
+		$db->where('t_prdct.cmpny_id', $companyID);
+		$db->where('t_prdct.id', $product_id);
+		$db->update('t_prdct', $productArray);
 	}
 }
